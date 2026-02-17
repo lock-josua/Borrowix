77056f4134757dd73d8362ea6e9e6eed56d22527
Josua <josuacagampang34@gmail.com>
2026-02-17 11:07:39 +0800

    Minor modification

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
 
-Route::get('dashboard', function () {
+
+Route::get('/', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
