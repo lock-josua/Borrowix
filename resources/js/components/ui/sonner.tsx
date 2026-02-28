00e16bf00f3b4c3ce1101d72b8d9727d3b473f08
Josua <josuacagampang34@gmail.com>
2026-02-28 11:10:57 +0800

    Toast updated

--- resources/js/components/ui/sonner.tsx
+++ resources/js/components/ui/sonner.tsx
@@ -17,12 +17,13 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="top-center"
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
+        success: <CircleCheckIcon className="size-4 text-green-500" />,
+        info: <InfoIcon className="size-4 text-blue-500" />,
+        warning: <TriangleAlertIcon className="size-4 text-yellow-500" />,
+        error: <OctagonXIcon className="size-4 text-red-500" />,
+        loading: <Loader2Icon className="size-4 animate-spin text-blue-500" />,
       }}
       style={
         {
@@ -30,6 +31,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
           "--border-radius": "var(--radius)",
+          "--success-border": "2px solid var(--green-500)",
+          "--error-border": "2px solid var(--destructive)",
+          "--warning-border": "2px solid var(--yellow-500)",
+          "--info-border": "2px solid var(--blue-500)",
+
         } as React.CSSProperties
       }
       {...props}
